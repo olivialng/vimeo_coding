11a98caab1aaf220a72c54718e0494fd438b75ff
Olivia Ng <43549231+olivialng@users.noreply.github.com>
2019-11-13 23:55:28 -0500

    Delete images.js

--- src/components/images.js
+++
@@ -1,7 +0,0 @@
-export const images = [
-	"https://i.ibb.co/K5ZczYf/decentman.jpg",
-	"https://i.ibb.co/W03MyMD/fourthphase.jpg",
-	"https://i.ibb.co/m013D7G/fullmoon.png",
-	"https://i.ibb.co/CWhcjV0/viceversa.jpg",
-	"https://i.ibb.co/BZ5DLcZ/wilderpeople.jpg"
-]
